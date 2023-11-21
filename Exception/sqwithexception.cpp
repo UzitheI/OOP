17144fca78b8a91e3e79714445bf2816b0855641
Ujjwal Puri <ujjwalpuri221@gmail.com>
2023-11-21 19:41:30 +0545

    overloading insertion exertion

--- Exception/sqwithexception.cpp
+++ Exception/sqwithexception.cpp
@@ -2,23 +2,23 @@
 #include<math.h>
 using namespace std;
 int sqroot(double n){
-    if(n<0){
-        throw n;
-    }
-    else{
-        cout<<"the square root is "<<sqrt(n)<<endl;
-    }
-}
-
-int main(){
-    double n,r;
     try{
-        cout<<"Enter the value of n:"<<endl;
-        cin>>n;
-        r=sqroot(n);
+
+   if(n<0){
+    throw n;
+   }
+   else{
+    cout<<"the square root of n is"<<sqrt(n)<<endl;
+   }
     }
     catch(double n){
-        cout<<n<<"is a negative number"<<endl;
-
+        cout<<n<<"is a negative number, so it cannot have a square root"<<endl;
     }
+}
+int main(){
+    double n;
+    cout<<"Enter the number:"<<endl;
+    cin>>n;
+    sqroot(n);
+    return 0;
 }
