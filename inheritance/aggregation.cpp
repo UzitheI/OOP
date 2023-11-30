ba1863a2050f04c713db2e0523f41068d8c7c9e6
Ujjwal Puri <ujjwalpuri221@gmail.com>
2023-11-30 21:42:52 +0545

    aggregration

---
+++ inheritance/aggregation.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+class employee{
+    int id;
+    public:
+    void get(){
+        cout<<"Enter id:"<<endl;
+        cin>>id;
+    }
+    void show(){
+        cout<<"The id is "<<id<<endl;
+    }
+};
+class company{
+    int comp;
+    employee e;
+    public:
+    void getdata(){
+        cout<<"Enter comp data:"<<endl;
+        cin>>comp;
+        e.get();
+    }
+    void showdata(){
+        cout<<"The comp is "<<comp<<endl;
+        e.show();
+    }
+};
+int main(){
+    company c1;
+    c1.getdata();
+    c1.showdata();
+    return 0;
+}
