5eef96473d1a8d3518eb977e7608320a5965fdeb
Ujjwal Puri <ujjwalpuri221@gmail.com>
2023-11-30 13:54:20 +0545

    using objects as function argument

---
+++ class/objectsasfunc.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+class height{
+    private:
+    int feet;
+    int inches;
+    public:
+    void getdata(){
+        cout<<"Enter feet:"<<endl;
+        cin>>feet;
+        cout<<"Enteer inches:"<<endl;
+        cin>>inches;
+    }
+    void addheight(height h1, height h2){
+        feet=h1.inches+h2.inches;
+        inches=inches%12;
+        feet+=h1.feet+h2.feet;
+
+    }
+    void show(){
+        cout<<"feet:"<<feet<<" inches:"<<inches<<endl;
+    }
+};
+int main(){
+    height h1;
+    h1.getdata();
+    height h2;
+    h2.getdata();
+    height h3;
+    h3.addheight(h1,h2);
+    h3.show();
+    return 0;
+}
