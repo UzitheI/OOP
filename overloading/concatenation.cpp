666b654e801a2aca88f4a2cc0aa7241b32bcf3a2
Ujjwal Puri <ujjwalpuri221@gmail.com>
2023-12-01 05:53:16 +0545

    concatenation

---
+++ overloading/concatenation.cpp
@@ -0,0 +1,20 @@
+// #include<iostream>
+// #include<string.h>
+// using namespace std;
+// class Home{
+//     string* s;
+//     string* b;
+//     public:
+//     Home(){
+
+//     }
+//     void getdata(){
+//         cout<<"enter teh 1st string"<<endl;
+//         cin>>s;
+//         cout<<"Enter the 2nd string:"<<endl;
+//         cin>>b;
+//     }
+//     Home operator +(const string b){
+//         s=strcat(s,b);
+//     }
+// }
